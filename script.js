88a02be043ad945d970401c869d1ec5a64e35b65
vickyz9555 <91750499+vickyz9555@users.noreply.github.com>
2021-10-01 12:53:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Vicky Zheng"; // update the part BETWEEN the "quotes"
+let userName = "vickyz9555"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
